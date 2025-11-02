a4a6589dd8857c65a8055513b93e9e7d0f9d0aca
alenaklad <alena.kladko@bk.ru>
2025-11-02 18:57:17 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://azhjsyaolfogmuyulwug.supabase.co'
+const supabaseUrl = 'https://azhjsyaolfoqmuyulwug.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6aGpzeWFvbGZvcW11eXVsd3VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4MjE5OTMsImV4cCI6MjA3NzM5Nzk5M30.f73plY5hXoo0er5I0CUC74Ijmd1FRApUK7Vp-LYGtZc'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
